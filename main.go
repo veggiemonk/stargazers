4cee6c832d8ae2debe481da8f8dd17386f8e5cef
Serhii Kuts <sergeykuc@gmail.com>
2018-02-13 11:02:44 +0200

    Update main.go

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/spencerkimball/stargazers/cmd"
+	"github.com/drmegavolt/stargazers/cmd"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
