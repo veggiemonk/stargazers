d4d550dface2c5a2fd9cf91d299336253fda8125
Serhii Kuts <sergeykuc@gmail.com>
2018-02-13 11:00:58 +0200

    Update analyze.go

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -20,8 +20,8 @@ import (
 	"errors"
 	"log"
 
-	"github.com/spencerkimball/stargazers/analyze"
-	"github.com/spencerkimball/stargazers/fetch"
+	"github.com/drmegavolt/stargazers/analyze"
+	"github.com/drmegavolt/stargazers/fetch"
 	"github.com/spf13/cobra"
 )
 
